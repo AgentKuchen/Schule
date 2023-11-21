4af40e1cae423d766ab2d51f75573bfd1ca48125
Agent_cake <agent.cake.hd@gmail.com>
2023-11-21 21:32:02 +0100

    added hangman

---
+++ hangman/Hangman/Klassen/FileWriter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hangman.Klassen
+{
+    public class FileWriter
+    {
+
+        public static void Write(string name, int score)
+        {
+            string filename = @"";
+            using (StreamWriter sw = new StreamWriter(filename, true))
+            {
+                sw.WriteLine($"{name},{score}");
+            }
+
+        }
+    }
+}
---
+++ hangman/Hangman/MainWindow.xaml.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Hangman
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string word = "";
+        string hiddenword = "";
+        int lives = 6;
+        int points = 0;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitializeWord();
+        }
+
+
+        private void InitializeWord()
+        {
+            word = "kuchen";
+           
+            for (int i = 0; i < word.Length; i++)
+                hiddenword += "_";
+
+            UpdateText();
+
+        }
+        
+        private void UpdateText()
+        {
+            text.Text = "";
+            for(int i = 0; i < hiddenword.Length; i++)
+            text.Text += hiddenword[i]+" ";
+
+            livestxt.Text = lives.ToString();
+        }
+
+
+        private void ChooseLetter(object sender, string letter)
+        {
+            if (word.Contains(letter))
+                Replaceletter(letter);
+            else
+                lives--;
+
+            if (sender is Button clickedButton)
+            {
+               
+                clickedButton.Visibility = Visibility.Collapsed;
+            }
+
+            UpdateText();
+
+        }
+        // funktioniert nicht
+        private void Replaceletter(string letter)
+        {
+            for (int i = 0; i < hiddenword.Length; i++)
+            {
+                if (letter == word[i].ToString())
+                    hiddenword[i].ToString().Replace("_", letter);
+            }
+                
+        }
+
+        private void a_Click(object sender, RoutedEventArgs e)
+        {
+            ChooseLetter(sender, "a");
+        }
+        
+         
+        private void b_Click(object sender, RoutedEventArgs e)
+        {
+            ChooseLetter(sender,"b");
+        }
+        
+        private void c_Click(object sender, RoutedEventArgs e)
+        {
+            ChooseLetter(sender, "c");
+        }
+
+        private void d_Click(object sender, RoutedEventArgs e)
+        {
+            ChooseLetter(sender, "d");
+        }
+
+        private void e_Click(object sender, RoutedEventArgs e)
+        {
+            ChooseLetter(sender, "e");
+        }
+
+        private void f_Click(object sender, RoutedEventArgs e)
+        {
+            ChooseLetter(sender, "f");
+        }
+
+        private void g_Click(object sender, RoutedEventArgs e)
+        {
+            ChooseLetter(sender, "g");
+        }
+
+        private void h_Click(object sender, RoutedEventArgs e)
+        {
+            ChooseLetter(sender, "h");
+        }
+
+        private void i_Click(object sender, RoutedEventArgs e)
+        {
+            ChooseLetter(sender, "i");
+        }
+
+        private void j_Click(object sender, RoutedEventArgs e)
+        {
+            ChooseLetter(sender, "j");
+        }
+
+        private void k_Click(object sender, RoutedEventArgs e)
+        {
+            ChooseLetter(sender, "k");
+        }
+
+        private void l_Click(object sender, RoutedEventArgs e)
+        {
+            ChooseLetter(sender, "l");
+        }
+
+        private void m_Click(object sender, RoutedEventArgs e)
+        {
+            ChooseLetter(sender, "m");
+        }
+
+        private void n_Click(object sender, RoutedEventArgs e)
+        {
+            ChooseLetter(sender, "n");
+        }
+
+        private void o_Click(object sender, RoutedEventArgs e)
+        {
+            ChooseLetter(sender, "o");
+        }
+
+        private void p_Click(object sender, RoutedEventArgs e)
+        {
+            ChooseLetter(sender, "p");
+        }
+
+        private void q_Click(object sender, RoutedEventArgs e)
+        {
+            ChooseLetter(sender, "q");
+        }
+
+        private void r_Click(object sender, RoutedEventArgs e)
+        {
+            ChooseLetter(sender, "r");
+        }
+
+        private void s_Click(object sender, RoutedEventArgs e)
+        {
+            ChooseLetter(sender, "s");
+        }
+
+        private void t_Click(object sender, RoutedEventArgs e)
+        {
+            ChooseLetter(sender, "t");
+        }
+
+        private void u_Click(object sender, RoutedEventArgs e)
+        {
+            ChooseLetter(sender, "u");
+        }
+
+        private void v_Click(object sender, RoutedEventArgs e)
+        {
+            ChooseLetter(sender, "v");
+        }
+
+        private void w_Click(object sender, RoutedEventArgs e)
+        {
+            ChooseLetter(sender, "w");
+        }
+
+        private void x_Click(object sender, RoutedEventArgs e)
+        {
+            ChooseLetter(sender, "x");
+        }
+
+        private void y_Click(object sender, RoutedEventArgs e)
+        {
+            ChooseLetter(sender, "y");
+        }
+
+        private void z_Click(object sender, RoutedEventArgs e)
+        {
+            ChooseLetter(sender, "z");
+        }
+        
+    }
+}
